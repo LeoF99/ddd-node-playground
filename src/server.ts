c870215c3cb32e84790449fcac7cdcfffd55225f
LeoF99 <leo.ferreira@dcx.ufpb.br>
2021-05-18 10:42:10 -0300

    feat: add .env Changes to be committed: 	modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import traceability from 'traceability';
+import dotenv from 'dotenv';
 
 import {
   swaggerAppCustomizer,
@@ -10,10 +11,15 @@ import HelloController from './application/controllers/hello.controller';
 import HomeController from './application/controllers/home.controller';
 import App from './app';
 
+dotenv.config();
+
 const { ContextAsyncHooks } = traceability;
+const {
+  PORT
+} = process.env;
 
 const app = new App({
-  port: 5000,
+  port: Number(PORT || 3333),
   controllers: [new HomeController(), new HelloController()],
   middleWares: [
     express.json(),
